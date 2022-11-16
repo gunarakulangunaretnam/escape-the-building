b2b093370ff50ff8ab70de7170edfdfb87e09257
Sajeeth Ahmad <sajiiahd@gmail.com>
2022-11-16 17:20:09 +0530

    fix: Add UI text appear

---
+++ Assets/scripts/UITextAppear.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextAppear : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    private void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(false);
+
+        }
+    }
+
+
+
+
+}
